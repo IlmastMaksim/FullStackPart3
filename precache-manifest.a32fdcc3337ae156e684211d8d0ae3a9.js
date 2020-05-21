a49a48c0e10e218d626eb721200e83a6920503a6
IlmastMaksim <ilmastm9@gmail.com>
2020-05-21 13:55:12 +0300

    uibuild

---
+++ precache-manifest.a32fdcc3337ae156e684211d8d0ae3a9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "654fbb2e5f0566849b6a3395cd174783",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c0f90cf7a6e9aae2a064",
+    "url": "/static/js/2.579390cc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.579390cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbeb0a00f1db99716ae3",
+    "url": "/static/js/main.8a44c376.chunk.js"
+  },
+  {
+    "revision": "bccee6d650832870a352",
+    "url": "/static/js/runtime-main.5401d62a.js"
+  }
+]);
